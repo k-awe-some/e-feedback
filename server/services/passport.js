de9a663642e9b8e1457a6352f029c795c2410c57
kay <kaynguyen.dev@gmail.com>
2019-09-27 14:09:17 -0400

    serialized & deserialized user with passport

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,7 +3,19 @@ const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const mongoose = require("mongoose");
 const keys = require("../config/keys");
 
-const User = mongoose.model("user"); // pulls out User Model Class
+const User = mongoose.model("user");
+
+// Converts User Model Instance into an id //
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Converts id back into an Instance //
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(usesr => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
