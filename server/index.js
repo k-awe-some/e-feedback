916a581afbd424ff152a28a3e83be231f8c325da
kay <kaynguyen.dev@gmail.com>
2019-09-26 23:04:23 -0400

    config google auth

--- server/index.js
+++ server/index.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-// Creates Route Handlers //
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
 
 // Communicates with Node on which port traffic to watch for //
 const PORT = process.env.PORT || 5000;
