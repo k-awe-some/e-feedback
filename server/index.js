f4c9dd211eda60108835a6446d1de87f33c921d7
kay <kaynguyen.dev@gmail.com>
2019-09-27 09:35:43 -0400

    refactored server structure

--- server/index.js
+++ server/index.js
@@ -1,32 +1,8 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
+require("./services/passport");
 
 const app = express();
+require("./routes/auth-routes")(app);
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: "/auth/google/callback"
-    },
-    (accessToken, refreshToken, profile) => {
-      console.log("🔓 access token", accessToken);
-      console.log("🔃 refresh token", refreshToken);
-      console.log("🎅🏻 profile", profile);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
-
-// Communicates with Node on which port traffic to watch for //
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
