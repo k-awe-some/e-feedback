9d9ac8d107bfaf0209f3677802a915792c266013
kay <kaynguyen.dev@gmail.com>
2019-09-28 18:55:26 -0400

    fixed duplicate prop bug; refactored

--- server/client/src/components/Header/Header.jsx
+++ server/client/src/components/Header/Header.jsx
@@ -2,39 +2,31 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 
-const Header = ({ auth }) => {
-  console.log(auth);
-
-  return (
-    <nav>
-      <div className="nav-wrapper">
-        <Link to={auth ? "/surveys" : "/"} className="left brand-logo">
-          eFeedback
-        </Link>
-        <ul className="right">
-          <li>
-            {auth ? (
-              <div>
-                <span>Hello, {auth.displayName}</span>
-                <a className="waves-effect waves-light btn" href="/api/logout">
-                  Log out
-                </a>
-              </div>
-            ) : (
-              <a
-                className="waves-effect waves-light btn"
-                href="/api/logout"
-                href="/auth/google"
-              >
-                Log in with Google
+const Header = ({ auth }) => (
+  <nav>
+    <div className="nav-wrapper">
+      <Link to={auth ? "/surveys" : "/"} className="left brand-logo">
+        eFeedback
+      </Link>
+      <ul className="right">
+        <li>
+          {auth ? (
+            <div>
+              <span>Hello, {auth.displayName}</span>
+              <a className="waves-effect waves-light btn" href="/api/logout">
+                Log out
               </a>
-            )}
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+            </div>
+          ) : (
+            <a className="waves-effect waves-light btn" href="/auth/google">
+              Log in with Google
+            </a>
+          )}
+        </li>
+      </ul>
+    </div>
+  </nav>
+);
 
 const mapStateToProps = ({ auth }) => ({ auth });
 
