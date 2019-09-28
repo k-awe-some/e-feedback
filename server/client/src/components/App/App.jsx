a176a5553d9245e9141db1692b8a95c001386bea
kay <kaynguyen.dev@gmail.com>
2019-09-28 16:23:25 -0400

    added action creator: fetchUser(); hooked redux thunk up to App

--- server/client/src/components/App/App.jsx
+++ server/client/src/components/App/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { connect } from "react-redux";
+import * as actions from "../../redux/actions"; // 'actions' as obj
 
 import Header from "../Header/Header";
 import Landing from "../Landing/Landing";
 import Dashboard from "../Dashboard/Dashboard";
 import SurveyNew from "../SurveyNew/SurveyNew";
 
-const App = () => {
+const App = ({ fetchUser }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <div className="container">
       <Router>
@@ -21,4 +29,7 @@ const App = () => {
   );
 };
 
-export default App;
+export default connect(
+  null,
+  actions
+)(App);
