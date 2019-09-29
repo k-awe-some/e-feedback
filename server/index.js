3c85a71b20db7de1a97845845d6b659405135c2a
kay <kaynguyen.dev@gmail.com>
2019-09-29 12:09:04 -0400

    handled requests in production on server; customized heroku build process

--- server/index.js
+++ server/index.js
@@ -28,5 +28,18 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+/***** HANDLING REQUESTS IN PRODUCTION *****/
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up main.js / main.css
+  app.use(express.static("client/build"));
+
+  // Express will serve up index.html
+  // if does not recognize the route after all attempts above
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
