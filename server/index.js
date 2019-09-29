4e40631195ae7808b2876465de51876e7b5cd8b6
kay <kaynguyen.dev@gmail.com>
2019-09-29 14:05:04 -0400

    set up Survey model class & Recipient subdoc

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const cookieSession = require("cookie-session");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 const keys = require("./config/keys");
-require("./models/user");
+require("./models/User");
+require("./models/Survey");
 require("./services/passport");
 
 mongoose.connect(keys.mongoURI, {
