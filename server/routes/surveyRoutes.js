5f5e2e985271c48f39287f5687d384708ce6e796
kay <kaynguyen.dev@gmail.com>
2019-09-29 21:21:34 -0400

    saved sent surveys into mongoDB; updated user credits

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("survey");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     // Create a survey
@@ -22,6 +22,15 @@ module.exports = app => {
 
     // Send an email
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      const user = await req.user.save(); // user with updated number of credits
+      res.send(user);
+    } catch (error) {
+      res.status(422).send(error);
+    }
   });
 };
