a7b8cfad1e6d3c5e784690484db0e471cfe73c98
kay <kaynguyen.dev@gmail.com>
2019-09-29 21:05:00 -0400

    improved html markup in surveyTemplate

--- server/services/emailTemplates/surveyTemplate.js
+++ server/services/emailTemplates/surveyTemplate.js
@@ -1,5 +1,21 @@
 // exports a function that takes a survey
 // and returns a string that represents html
 module.exports = survey => {
-  return `<div>${survey.body}</div>`;
+  return `
+  <html>
+    <body>
+      <div style="text-align: center">
+        <h3>We'd like your input so we can serve you better!</h3>
+        <p>Your answer to the following question would mean a lot to us:</p>
+        <p>${survey.body}</p>
+        <div>
+          <a href="http://localhost:3000">Yes</a>
+        </div>
+        <div>
+          <a href="http://localhost:3000">No</a>
+        </div>
+      </div>
+    </body>
+  </html>
+  `;
 };
