ef1bcc11aca0666bd3302a1e75c4fae88564288b
kay <kaynguyen.dev@gmail.com>
2019-10-03 08:34:42 -0400

    retouched dashboard; added routing to surveys/new

--- server/client/src/components/Dashboard/Dashboard.jsx
+++ server/client/src/components/Dashboard/Dashboard.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Dashboard = () => <h2>DASHBOARD</h2>;
+const Dashboard = () => (
+  <div>
+    <h2>DASHBOARD</h2>{" "}
+    <div class="fixed-action-btn">
+      <Link
+        to="surveys/new"
+        class="btn-floating btn-large waves-effect waves-light red"
+      >
+        <i class="material-icons">add</i>
+      </Link>
+    </div>
+  </div>
+);
 
 export default Dashboard;
